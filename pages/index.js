ac2ad0dd4a0f66a4901af2ab1c73e1a18e96fabe
Jacob Stanger <devjake@outlook.com>
2021-10-27 11:02:01 -0600

    expand out horiz

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
     <NavbarMain />
-    <Container className="md-container">
+    <Container>
       <Head>
         <title>ReactJS with react-bootstrap</title>
         <link rel="icon" href="/favicon-32x32.png" />
@@ -82,17 +82,17 @@ export default function Home() {
         </Container>
       </Container>
 
-      <footer className="cntr-footer">
-        <a
-          href="https://vercel.com?filter=next.js&utm_source=github&utm_medium=example&utm_campaign=next-example"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="sml-logo" />
-        </a>
-      </footer>
     </Container>
-    </>
+    <footer className="cntr-footer">
+      <a
+        href="https://vercel.com?filter=next.js&utm_source=github&utm_medium=example&utm_campaign=next-example"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className="sml-logo" />
+      </a>
+    </footer>
+  </>
   )
 }
